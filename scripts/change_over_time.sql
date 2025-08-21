2e07069c86ce278d88ed3da59aafdeea88f04232
francesco-mt <francesco.bellia.w2@gmail.com>
2025-08-21 17:41:37 +0200

    Create change_over_time.sql

---
+++ scripts/change_over_time.sql
@@ -0,0 +1,60 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+*/
+-- Analyse sales performance over time
+
+SELECT
+    YEAR(order_date) AS order_year,
+    CASE MONTH(order_date)
+        WHEN 1 THEN 'January'
+        WHEN 2 THEN 'February'
+        WHEN 3 THEN 'March'
+        WHEN 4 THEN 'April'
+        WHEN 5 THEN 'May'
+        WHEN 6 THEN 'June'
+        WHEN 7 THEN 'July'
+        WHEN 8 THEN 'August'
+        WHEN 9 THEN 'September'
+        WHEN 10 THEN 'October'
+        WHEN 11 THEN 'November'
+        WHEN 12 THEN 'December'
+	END AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+
+
+-- Same as above but with a unique date column
+
+SELECT
+    DATETRUNC(month, order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date);
+
+
+
+-- Same as above but with a different format 
+
+SELECT
+    FORMAT(order_date, 'yyyy MMM') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy MMM')
+ORDER BY FORMAT(order_date, 'yyyy MMM');
+  
+  
