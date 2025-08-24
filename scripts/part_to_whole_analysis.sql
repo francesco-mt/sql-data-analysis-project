ab77a4ffb2e15fe03fb59209fcd7c46495a5fb43
francesco-mt <francesco.bellia.w2@gmail.com>
2025-08-24 17:03:59 +0200

    Create part_to_whole_analysis.sql

---
+++ scripts/part_to_whole_analysis.sql
@@ -0,0 +1,16 @@
+-- Shows the contribution of each category to the total revenue
+WITH category_totals AS (
+    SELECT 
+        p.category,
+        SUM(s.sales_amount) AS total_revenue_by_category
+    FROM gold.fact_sales s
+    LEFT JOIN gold.dim_products p ON s.product_key = p.product_key
+    GROUP BY p.category
+)
+SELECT 
+	SUM(total_revenue_by_category) OVER() as total_revenue,
+    category,
+	total_revenue_by_category,
+	ROUND(CAST(total_revenue_by_category AS FLOAT)/ SUM(total_revenue_by_category) OVER() * 100,2) as part_to_whole_percentage
+FROM category_totals
+ORDER BY part_to_whole_percentage DESC;
